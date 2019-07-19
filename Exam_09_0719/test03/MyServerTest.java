9cfd3a50f9d69f8764a69dc5874c8f014b6bcf9f
kokamoto01 <k017c1355@it-neec.jp>
2019-07-19 11:54:35 +0900

    20190719 ServerSocket

---
+++ Exam_09_0719/test03/MyServerTest.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.net.*;
+
+public class MyServerTest{
+    public static void main(String[] args){
+        new MyServerClass().mainProc();
+    }
+}
+
+class MyServerClass{
+	ServerSocket server;
+	Socket socket;
+	ClientWorker worker;
+	int socketNumber;
+	void mainProc(){
+		try {
+			server = new ServerSocket(12345);
+			while (true){
+				socket = server.accept();
+				//クライアントごとのオブジェクトの作成とマルチスレッド処理の実行
+				worker = new ClientWorker(socket, ++socketNumber);
+				worker.start();
+			}
+		} catch (IOException err){
+			System.out.println(err);
+		}
+	}
+}
+
+class ClientWorker extends Thread{
+	Socket socket;
+	BufferedWriter bw;
+	BufferedReader br;
+	BufferedReader brToSocket;
+	String buf;
+	int socketNumber;
+	int messageCount;
+	ClientWorker(Socket socket, int socketNumber){	//ソケットオブジェクトとソケット番号の保存
+		this.socket = socket;
+		this.socketNumber = socketNumber;
+	}
+	public void run(){							//マルチスレッドとなる処理
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream("index.html")));
+			brToSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			while(true){
+				buf = brToSocket.readLine();
+				if(buf.equals("\r\n")){
+					// 空行判定するときにここからどうすればいい？
+
+				}
+				if(buf == null){
+					break;
+				}
+				System.out.println(buf);
+			}
+			System.out.println("test");
+			while (true){
+				buf = br.readLine();
+				if (buf == null){
+					break;
+				}
+				bw.write(buf);
+				bw.newLine();
+				bw.flush();
+			}
+			socket.close();
+			// メッセージを送り終えたらsocketを閉じる
+		} catch (IOException err){
+			System.out.println(err);
+		}
+	}
+}
+
